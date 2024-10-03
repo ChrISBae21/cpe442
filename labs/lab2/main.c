c0d6e80d046f66146afb727b13838b4c78aa3b03
ChrISBae21 <cbgnote@gmail.com>
2024-10-03 11:30:07 -0700

    kinda works

--- labs/lab2/main.c
+++ labs/lab2/main.c
@@ -1,3 +1,32 @@
-void main() {
-    
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Usage: %s <image_path>\n", argv[0]);
+        return -1;
+    }
+
+    // Read the image file using C++ style cv::imread function
+    cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR);
+
+    // Check for failure
+    if (image.empty()) {
+        printf("Error: Could not open or find the image.\n");
+        return -1;
+    }
+
+    cv::Mat resized_image;
+    cv::resize(image, resized_image, cv::Size(500, 500));
+
+    // Create a window
+    cv::namedWindow("Image Viewer", cv::WINDOW_AUTOSIZE);
+
+    // Show the image inside the window
+    cv::imshow("Image Viewer", resized_image);
+
+    // Wait for any keystroke in the window
+    cv::waitKey(0);
+
+    return 0;
 }
