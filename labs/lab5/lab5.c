cd834688773d975e6b8487a727da9bb128afbc60
ChrISBae21 <cbgnote@gmail.com>
2024-11-04 23:27:59 -0800

    need to test sobel vector

--- labs/lab5/lab5.c
+++ labs/lab5/lab5.c
@@ -1,7 +1,7 @@
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <pthread.h>
-#include <atomic>
+#include <arm_neon.h>
 
 #define RED_WEIGHT      0.2126
 #define GREEN_WEIGHT    0.7152
@@ -28,7 +28,9 @@ void *thread_function(void *arg);
 void *main_thread(void *arg);
 void to442_grayscale(int id, int numRows);
 void to442_sobel(int id, int numRows);
-
+void load_and_convert(uint8_t* rows[3], uint8x8_t pixelVect_u8[3][3], int16x8_t pixelVect_s16[3][3], int x);
+uint8x8_t clip_and_convert(int16x8_t mag);
+void apply_filter(int16x8_t* sumX, int16x8_t* sumY, int16x8_t pixelVect_s16[3][3]);
 
 void *thread_function(void *arg) {
     int id = *((int*)arg);
@@ -59,11 +61,84 @@ void to442_grayscale(int id, int numRows) {
     }
 }
 
-void to442_sobel(int id, int numRows) {
-    int sumX, sumY, mag;
-    int start = id * numRows;
-    int end = (id == NUM_THREADS - 1) ? frame_info.gray.rows - 1: start + numRows;
+// void to442_grayscale(int id, int numRows) {
+//     int x, y;
+//     int start = id * numRows;
+//     int end = (id == NUM_THREADS - 1) ? frame_info.gray.rows : start + numRows;
 
+//     uint8_t* row;
+
+//     uint8x8_t blue;
+//     uint8x8_t green;
+//     uint8x8_t red;
+//     // cv::Vec3b* rowColor;
+//     uint8x8x3_t pixels;
+
+//     for (y = start; y < end; y++) {
+//         // cv::Vec3b* rowColor = colorImage.ptr<cv::Vec3b>(y);
+
+//         rows = frame_info.frame.ptr<uint8_t>(y);
+
+//         for (x = 0; x < frame_info.frame.cols-8; x+=8) {
+//             pixels = vld3_u8(&row[x * 8]);
+
+//             blue = pixels.val[0];  
+//             green = pixels.val[1]; 
+//             red = pixels.val[2];   
+
+//             /* TODO */
+
+//             cv::Vec3b pixel = frame_info.frame.at<cv::Vec3b>(y, x);
+//             uint8_t grayValue = static_cast<uint8_t>(
+//                 pixel[2] * RED_WEIGHT + pixel[1] * GREEN_WEIGHT + pixel[0] * BLUE_WEIGHT
+//             );
+//             frame_info.gray.at<uint8_t>(y, x) = grayValue;
+//         }
+//     }
+// }
+
+void to442_sobel(int id, int numRows) {
+    uint8_t* rows[3];
+    uint8x8_t pixelVect_u8[3][3];
+    int16x8_t pixelVect_s16[3][3];
+
+    uint8_t* sobel;
+
+    int16x8_t sumX, sumY, mag;
+    uint8x8_t mag_u8;
+
+    int start;
+    int end;
+
+    start = id * numRows;
+    if(id == 0) start++;
+
+    end = (id == NUM_THREADS - 1) ? frame_info.gray.rows - 1: start + numRows;
+
+    for (int y = start; y < end; y++) {
+
+        rows[0] = frame_info.gray.ptr<uint8_t>(y - 1);
+        rows[1] = frame_info.gray.ptr<uint8_t>(y);
+        rows[2] = frame_info.gray.ptr<uint8_t>(y + 1);
+        sobel = frame_info.sobel.ptr<uint8_t>(y);
+
+        for (int x = 1; x < frame_info.gray.cols - 8 - 1; x+=8) {
+            load_and_convert(rows, pixelVect_u8, pixelVect_s16, x);    
+            apply_filter(&sumX, &sumY, pixelVect_s16);                                                                 
+
+            /* Calculate magnitude */
+            sumX = vabsq_s16(sumX);
+            sumY = vabsq_s16(sumY);
+            mag = vaddq_s16(sumX, sumY);
+            mag_u8 = clip_and_convert(mag);
+
+            vst1_u8(&sobel[x], mag_u8);
+        }
+    }
+}
+
+void apply_filter(int16x8_t* sumX, int16x8_t* sumY, int16x8_t pixelVect_s16[3][3]) {
+    int row, col;
     int Gx[3][3] = {
         {-1, 0, 1},
         {-2, 0, 2},
@@ -75,30 +150,46 @@ void to442_sobel(int id, int numRows) {
         { 1,  2,  1}
     };
 
-    if(id == 0) start++;
+    for(row = 0; row < 3; row++) {
+        for(col = 0; col < 3; col++) {
+            *sumX = vmlaq_n_s16(*sumX, pixelVect_s16[row][col], Gx[row][col]);
+            *sumX = vmlaq_n_s16(*sumY, pixelVect_s16[row][col], Gy[row][col]);
+        }
+    }
 
-    for (int y = start; y < end; y++) {
-        for (int x = 1; x < frame_info.gray.cols - 1; x++) {
-            /* convolve on the x */
-            sumX =  (frame_info.gray.at<uint8_t>(y-1, x-1) * Gx[0][0]) + (frame_info.gray.at<uint8_t>(y-1, x+1) * Gx[0][2]) +
-                    (frame_info.gray.at<uint8_t>(y, x-1)   * Gx[1][0]) + (frame_info.gray.at<uint8_t>(y, x+1)   * Gx[1][2]) +
-                    (frame_info.gray.at<uint8_t>(y+1, x-1) * Gx[2][0]) + (frame_info.gray.at<uint8_t>(y+1, x+1) * Gx[2][2]);
-            
-            /* convolve on the y */
-            sumY =  (frame_info.gray.at<uint8_t>(y-1, x-1) * Gy[0][0]) + (frame_info.gray.at<uint8_t>(y-1, x)   * Gy[0][1]) +
-                    (frame_info.gray.at<uint8_t>(y-1, x+1) * Gy[0][2]) + (frame_info.gray.at<uint8_t>(y+1, x-1) * Gy[2][0]) +
-                    (frame_info.gray.at<uint8_t>(y+1, x)   * Gy[2][1]) + (frame_info.gray.at<uint8_t>(y+1, x+1) * Gy[2][2]);
+}
 
-            mag = std::abs(sumX) + std::abs(sumY);
+/* check if passing pixelVect_s16 works as reference */
+void load_and_convert(uint8_t* rows[3], uint8x8_t pixelVect_u8[3][3], int16x8_t pixelVect_s16[3][3], int x) {
+    int row, col;
+    for (row = 0; row < 3; row++) {
+        pixelVect_u8[row][0] = vld1_u8(&rows[row][x - 1]); /* Load left pixel */
+        pixelVect_u8[row][1] = vld1_u8(&rows[row][x]);     /* Load center pixel */
+        pixelVect_u8[row][2] = vld1_u8(&rows[row][x + 1]); /* Load right pixel */
+    }
 
-            if(mag > 255) {
-                mag = 255;
-            }
-            frame_info.sobel.at<uint8_t>(y, x) = (uint8_t)mag;
+    /* Convert to 16-bit vectors */
+    for (row = 0; row < 3; row++) {
+        for (col = 0; col < 3; col++) {
+            pixelVect_s16[row][col] = vreinterpretq_s16_u16(vmovl_u8(pixelVect_u8[row][col])); /* Expand to 16-bit */
         }
     }
 }
 
+uint8x8_t clip_and_convert(int16x8_t mag) {
+    int16x8_t clampZero, clamp255;
+    uint8x8_t result;
+
+    /* clamp negative values to 0 */
+    clampZero = vmaxq_s16(mag, vdupq_n_s16(0));
+    /* clamp values > 255 to 255 */
+    clamp255 = vminq_s16(clampZero, vdupq_n_s16(255));
+    /* convert to uint8x8_t */
+    result = vqmovn_u16(vreinterpretq_u16_s16(clamp255));
+    
+
+    return result;
+}
 
 int main(int argc, char** argv) {
     if (argc != 2) {
